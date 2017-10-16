b02db6344e585bd13c67b530876e737ea02dc634
amd <amd@ubu1604>
2017-10-16 19:24:29 +0200

    installer rellotge

--- release/Rellotge/setup.sh
+++ release/Rellotge/setup.sh
@@ -11,7 +11,7 @@ cat ./lxsession.autostart > /home/pi/.config/lxsession/LXDE-pi/autostart
 #Configure CronTab
 echo "*/1 * * * * export DISPLAY=:0.0 && /home/pi/Desktop/Rellotge/watchdog.sh" >> mycron
 echo "*/10 * * * * export DISPLAY=:0.0 && /home/pi/Desktop/SensorSync/sensorsync.sh" >> mycron
-echo "0 30 * * * export DISPLAY=:0.0 && /home/pi/Desktop/SensorSync/readLocalDB.sh" >> mycron
+echo "0 15 * * * export DISPLAY=:0.0 && /home/pi/Desktop/SensorSync/readLocalDB.sh" >> mycron
 echo "1 0 * * * export DISPLAY=:0.0 && /home/pi/Desktop/SensorSync/photodownload.sh" >> mycron
 echo "0 0 * * * sudo reboot" >> mycron
 #install new cron file
