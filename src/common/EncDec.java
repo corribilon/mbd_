518435fb4cb835105a32b46cc5b54366fd9f2720
amd <amd@ubu1604>
2018-09-17 21:20:01 +0200

    Programa de encriptacion y desencriptacion

--- src/common/EncDec.java
+++ src/common/EncDec.java
@@ -1,4 +1,8 @@
 package common;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 import org.apache.commons.codec.binary.Base64;
@@ -9,11 +13,78 @@ public class EncDec {
 	private static final Logger LOGGER = Tracer.getLogger(EncDec.class);
 
 	public static void main(String[] args) {
-		test("Hello?¿?");		
+		
+		@SuppressWarnings("resource")
+		Scanner s = new Scanner(System.in);
+		
+		while(true) {
+		
+			
+			
+			System.out.println("+---------------------------+");
+			System.out.println("| CODIFICADOR/DECODIFICADOR |");
+			System.out.println("+---------------------------+");
+			
+			System.out.println("");
+			
+			System.out.println("1) Codificar");
+			System.out.println("2) Decodificar");
+			
+			System.out.println("Escriba un opción (1 o 2):");
+			
+			int opt = Integer.parseInt(s.nextLine());
+			
+			boolean isOk = true;
+			
+			if(opt!=1 && opt!=2) {
+				System.out.println("Opcion incorrecta. Saliendo del programa!");
+				isOk = false;
+			}
+			
+			if(isOk) {
+			
+				System.out.println("Escribe el texto:");
+				
+				String str = s.nextLine();			
+				
+				String res = "";
+				
+				if(opt==1) {
+					res = enc(str);
+					System.out.println(enc(str));
+				}else {
+					res = dec(str);
+					System.out.println(dec(str));
+				}
+				
+				File f = new File("out.txt");
+				PrintWriter pw;
+				try {
+					pw = new PrintWriter(f);
+					pw.println(res);
+					pw.close();
+					System.out.println("");
+					System.out.println(" --> Resultado se ha guardado en el fichero: out.txt");
+				} catch (FileNotFoundException e) {
+					System.out.println("No se pudo generar el fichero out.txt. Revise los permisos de la carpeta "
+							+ "que contiene este ejecutable.");
+					e.printStackTrace();
+				}
+				
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					
+	//				e.printStackTrace();
+				}
+			
+			}
+			
+		}
 	}
 
 
-	private static void test(String stringToEnc) {
+	public static void test(String stringToEnc) {
 		String enc = enc(stringToEnc);
 		String dec = dec(enc);
 		
